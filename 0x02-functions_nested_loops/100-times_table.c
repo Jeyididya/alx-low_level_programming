8a90a98048c447b96acf5d17a6c7ad9a2071711a
Yididya <jedidiahandualem@gmail.com>
2022-04-12 09:46:32 -0700

    fixed new typo

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * Prints the times table of the input starting with 0.
+ * print_times_table - Prints the times table of the input starting with 0.
+ * @n: time to be printed
  */
 void print_times_table(int n)
 {
