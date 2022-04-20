3d964dba0b4580cf96df9f51fa6ed93f62ee4a0d
Yididya <jedidiahandualem@gmail.com>
2022-04-20 11:43:37 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,10 +13,10 @@ int a = -1, i;
 for (i = 0; dest[i] != '\0'; i++);
 
 do {
-	a++;
-	dest[i] = src[a];
-	i++;
-} while (src[a] != '\0');
-
+a++;
+dest[i] = src[a];
+i++;
+}
+while (src[a] != '\0');
 return (dest);
 }
