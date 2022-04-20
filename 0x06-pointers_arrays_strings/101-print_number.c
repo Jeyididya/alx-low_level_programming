8463b886ba0365a22a0a90d9ad56a7f8f7e0a33c
Yididya <jedidiahandualem@gmail.com>
2022-04-20 12:56:43 -0700

    typo fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,14 +3,10 @@
 
 
 /**
- *
- *  * print_number - prints an integer.
- *
- *   * @n: input integer.
- *
- *    * Return: no return.
- *
- *     */
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
 
 void print_number(int n)
 {
