fb0cfed4fb0410a047fe231e9ec754a134b38f34
Yididya <jedidiahandualem@gmail.com>
2022-04-25 02:48:33 -0700

    task 9

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,23 @@
 #include "holberton.h"
 
+
+
 /**
- * set_string - sets the value of a pointer to a char
- * @s: pointer to pointer
- * @to: pointer char
- */
+ *
+ *  * set_string - sets the value of a pointer to a char
+ *
+ *   * @s: source adress.
+ *
+ *    * @to: target adress.
+ *
+ *     * Return: no return.
+ *
+ *      */
 
 void set_string(char **s, char *to)
+
 {
-*s = to;
+
+		*s = to;
+
 }
