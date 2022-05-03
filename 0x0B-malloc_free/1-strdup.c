807106453cdbed67f7d39ede720c5bd0530775b3
Yididya <jedidiahandualem@gmail.com>
2022-05-03 11:05:50 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,9 +13,11 @@ char *_strdup(char *str)
 {
 	char *strout;
 	unsigned int i, j;
+
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++);
+	for (i = 0; str[i] != '\0'; i++)
+		;
 	strout = (char *)malloc(sizeof(char) * (i + 1));
 	if (strout == NULL)
 		return (NULL);
