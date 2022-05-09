a0b676fe3721e07a268bc5f966b8fea5217142e2
Yididya <jedidiahandualem@gmail.com>
2022-05-09 06:54:41 -0700

    typo fix in dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,9 +6,7 @@
 
 /**
  * dog_t - Typedef for struct dog
- *
  */
-
 typedef struct dog dog_t;
 
 
@@ -18,6 +16,7 @@ typedef struct dog dog_t;
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ *
  * Description: struct called "dog" that stores its name, its age
  * and the name of its owner.
  */
@@ -25,25 +24,14 @@ typedef struct dog dog_t;
 struct dog
 
 {
-
-		char *name;
-
-			float age;
-
-				char *owner;
+	char *name;
+	float age;
+	char *owner;
 
 };
 
-
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
-
-
-
 #endif
